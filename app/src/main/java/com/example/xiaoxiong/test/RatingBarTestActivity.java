a43a01141be4f442af4ab0df21eac51355b6693c
wangyuchao <wangyuchao@do-global.com>
2018-10-12 12:40:57 +0800

    change: 添加RatingDialog

--- app/src/main/java/com/example/xiaoxiong/test/RatingBarTestActivity.java
+++ app/src/main/java/com/example/xiaoxiong/test/RatingBarTestActivity.java
@@ -1,8 +1,11 @@
 package com.example.xiaoxiong.test;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.RatingBar;
 
 import java.sql.SQLOutput;
@@ -20,22 +23,37 @@ public class RatingBarTestActivity extends AppCompatActivity {
     private RatingBar ratingBarTwo = null;
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rating_bar_test);
         initView();
+
+//        View ratingBar = getLayoutInflater().inflate(R.layout.dialog_rate,null);
+//        //final RatingBar ratingBar = new RatingBar(getApplicationContext());
+//        AlertDialog.Builder builder = new AlertDialog.Builder(RatingBarTestActivity.this);
+//        builder.setTitle("Rate US")
+//                .setView(ratingBar)
+//                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+//                    public void onClick(DialogInterface dialog, int which) {
+////                        setTitle("" + ratingBar.getRating());
+////                        float star = ratingBar.getRating();
+//                    }
+//                }).setNegativeButton("取消", null).show();
+
+        RatingDialog ratingDialog = new RatingDialog(this);
+        ratingDialog.show();
+
     }
 
     //初始化函数
-    public void initView(){
+    public void initView() {
 
         //获得控件
-        ratingBar1 = (RatingBar)findViewById(R.id.ratingBar1);
-        ratingBar2 = (RatingBar)findViewById(R.id.ratingBar2);
-        ratingBarOne = (RatingBar)findViewById(R.id.ratingBarOne);
-        ratingBarTwo = (RatingBar)findViewById(R.id.ratingBarTwo);
+        ratingBar1 = (RatingBar) findViewById(R.id.ratingBar1);
+        ratingBar2 = (RatingBar) findViewById(R.id.ratingBar2);
+        ratingBarOne = (RatingBar) findViewById(R.id.ratingBarOne);
+        ratingBarTwo = (RatingBar) findViewById(R.id.ratingBarTwo);
 
         //为ratingBar1添加 OnRatingBarChangeListener
         //当用户交互改变分值时，触发该事件
@@ -43,11 +61,11 @@ public class RatingBarTestActivity extends AppCompatActivity {
             @Override
             public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
                 //第一个参数，当前评分修改的 ratingBar
-                Log.d(TAG,"ratingBar: " + ratingBar);
+                Log.d(TAG, "ratingBar: " + ratingBar);
                 //第二个参数，当前评分分手，范围 0~星星数量
-                Log.d(TAG,"rating: " + rating);
+                Log.d(TAG, "rating: " + rating);
                 //第三个参数，如果评分改变是由用户触摸手势或方向键轨迹球移动触发的，则返回true
-                Log.d(TAG,"fromUser:" + fromUser);
+                Log.d(TAG, "fromUser:" + fromUser);
 
                 //设置不可交互的展示型ratingBarOne的评分分数
                 ratingBarOne.setRating(rating);
---
+++ app/src/main/java/com/example/xiaoxiong/test/RatingDialog.java
@@ -0,0 +1,74 @@
+package com.example.xiaoxiong.test;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+public class RatingDialog extends Dialog {
+
+    private ImageView img_back;
+    private RatingBar ratingBar;
+    private TextView textView;
+    private Button encourage;
+
+    public RatingDialog(Context context) {
+        super(context, R.style.Dialog);
+    }
+
+    public RatingDialog(Context context, int themeResId) {
+        super(context, themeResId);
+    }
+
+    protected RatingDialog(Context context, boolean cancelable, OnCancelListener cancelListener) {
+        super(context, cancelable, cancelListener);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+//        在dialog中的oncreate方法中，将
+//        setContentView(R.layout.link_type)改为
+//        View view = this.getLayoutInflater().inflate(R.layout.link_type, null);
+//        setContentView(view);
+//        而控件则由view.findViewById(R.id.d)得到
+
+        this.setContentView(R.layout.dialog_rate);
+
+        initView();
+
+        setCanceledOnTouchOutside(false);
+    }
+
+    public void initView(){
+        img_back = (ImageView) findViewById(R.id.close);
+        ratingBar = (RatingBar) findViewById(R.id.ratingBar);
+        textView = (TextView) findViewById(R.id.textview);
+        encourage = (Button) findViewById(R.id.encourage_button);
+        img_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RatingDialog.this.dismiss();
+            }
+        });
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                //第一个参数，当前评分修改的 ratingBar
+                //第二个参数，当前评分分手，范围 0~星星数量
+                //第三个参数，如果评分改变是由用户触摸手势或方向键轨迹球移动触发的，则返回true
+                textView.setVisibility(View.INVISIBLE);
+                encourage.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+
+
+}
