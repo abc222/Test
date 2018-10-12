757874f7c86c6ffdb8c17b4a649de6dd10bd7b80
wangyuchao <wangyuchao@do-global.com>
2018-10-12 12:40:57 +0800

    change: 添加RatingDialog

---
+++ app/src/main/java/com/example/xiaoxiong/test/DynamicThemeActivity.java
@@ -0,0 +1,13 @@
+package com.example.xiaoxiong.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DynamicThemeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dynamic_theme);
+    }
+}
